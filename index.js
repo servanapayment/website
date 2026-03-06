d8d4de803d9e841ad10afc5d5e034f1d638971b1
servanapayment <servanapayment@gmail.com>
2026-03-06 19:49:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Selamat datang di Frontend</h1>
+      <p>Buka <a href="/products">Daftar Produk</a> untuk melihat data dari backend.</p>
+    </div>
+  );
+}
