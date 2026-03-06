83d89a6cef98a3115099aaf4ce0a28cb9673bf53
servanapayment <servanapayment@gmail.com>
2026-03-06 19:55:08 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Selamat datang di Frontend</h1>
-      <p>Buka <a href="/products">Daftar Produk</a> untuk melihat data dari backend.</p>
-    </div>
-  );
-}
